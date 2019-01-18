66205178c15944b362e1eddca00539f7cc618dbf
Kirk Amundson <kramundson@ucdavis.edu>
2019-01-18 15:59:29 -0800

    nonredundant compare 2 chromosomes on server

--- 2019_0115_dosage_links.R
+++ 2019_0115_dosage_links.R
@@ -25,8 +25,8 @@ dosage_geno_comps <- dosage_geno_counts %>%
   setNames(paste0("target.", names(.))) %>% 
   crossing(dosage_geno_counts) %>%
   filter(target.bin != bin & target.dosage.gt != dosage.gt) %>%
-  mutate(bincomp = ifelse(target.bin > bin, paste(target.bin, target.dosage.gt, bin, dosage.gt, sep = "_"), paste(bin, dosage.gt, target.bin, target.dosage.gt, sep = "_"))) # %>% 
-# distinct(bincomp, .keep_all = T) # here, .keep_all=T specifies to keep all variables. Not doing this makes the computation take that much longer
+  mutate(bincomp = ifelse(target.bin > bin, paste(target.bin, target.dosage.gt, bin, dosage.gt, sep = "_"), paste(bin, dosage.gt, target.bin, target.dosage.gt, sep = "_"))) %>% 
+  distinct(bincomp, .keep_all = T) # here, .keep_all=T specifies to keep all variables. Not doing this makes the computation take that much longer
 
 
 dosage_geno_comps$inds.in.target <- rep(NA, nrow(dosage_geno_comps))
@@ -73,6 +73,12 @@ for (i in 1:nrow(fisher)) {
   fisher$src.bin.num[i] <- which(unique(dosage_genos$bin) == fisher$bin[i])
   fisher$target.bin.num[i] <- which(unique(dosage_genos$bin) == fisher$target.bin[i])
 }
+
+# for graphing purposes, may want to check src.bin.num and target.bin.num, and reverse source-target so that all tiles show up on the same side of the diagonal
+# tested this using a single chromosome, works. Only plots below the diagonal.
+fisher <- fisher %>%
+  mutate(src.bin.num.plot = ifelse(src.bin.num > target.bin.num, src.bin.num, target.bin.num)) %>%  # source plot always greater of src.bin.num and target.bin.num
+  mutate(target.bin.num.plot = ifelse(src.bin.num > target.bin.num, target.bin.num, src.bin.num)) # target plot always lesser of src.bin.num and target.bin.num
 
 # draw plot
 p <- ggplot(fisher, aes(x = src.bin.num, y = target.bin.num, color = fisher.pval)) +
